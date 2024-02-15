41ee154f3bf8e508152630369fb9fdf510ca3b79
Iryna Yehorova <interest4me@gmail.com>
2024-02-16 00:42:04 +0200

    fix: correct controllers' structure

--- src/http_server/websocket.ts
+++ src/http_server/websocket.ts
@@ -1,31 +1,44 @@
 import { WebSocketServer } from 'ws';
-import { RegRequest, Request } from '../types';
-// import { Buffer } from 'buffer';
-import userDb from '../database/users';
-import roomsDb from '../database/rooms';
-import { responseWrapper } from '../utils/responseWrapper';
+import {
+  AddShipsRequest,
+  AddUserToRoomRequest,
+  RegRequest,
+  Request,
+} from '../types';
+import { WebSocket } from 'ws';
+import registration from '../controllers/registration';
+import createRoom from '../controllers/createRoom';
+import addUserToRoom from '../controllers/addUserToRoom';
+import addShip from '../controllers/addShips';
+// import userDb from '../database/users';
+// import roomsDb from '../database/rooms';
+// import wsDb from '../database/wsDb';
+
 const PORT = 3000;
 export const wss = new WebSocketServer({ port: PORT });
+const clients = new Set<WebSocket>();
 
 wss.on('connection', function connection(ws) {
+  clients.add(ws);
   ws.on('message', function message(clientData) {
     const data = JSON.parse(clientData.toString()) as Request;
-    console.log('data', data);
-    const dataInfo = JSON.parse(data.data);
+
+    // console.log('ws', ws);
+    const dataInfo = data.type !== 'create_room' && JSON.parse(data.data);
+    console.log('dataInfo', dataInfo);
+    console.log(data.type);
     switch (data.type) {
       case 'reg':
-        const regResponce = userDb.addNewUser(dataInfo as RegRequest);
-        const updateRoomResponce = roomsDb.updateRoomState();
-        const updateWinREsponce = userDb.updateWiners();
-        ws.send(responseWrapper(regResponce, 'reg'));
-        ws.send(responseWrapper(updateRoomResponce, 'update_room'));
-        ws.send(responseWrapper(updateWinREsponce, 'update_winners'));
+        registration(ws, clients, dataInfo as RegRequest);
         break;
       case 'create_room':
+        createRoom(ws, clients);
         break;
       case 'add_user_to_room':
+        addUserToRoom(ws, clients, dataInfo as AddUserToRoomRequest);
         break;
       case 'add_ships':
+        addShip(ws, dataInfo as AddShipsRequest);
         break;
       case 'attack':
         break;
