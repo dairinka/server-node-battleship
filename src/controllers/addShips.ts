30a73b7dc216fadeb470cbac12292b170dabb68b
Iryna Yehorova <interest4me@gmail.com>
2024-02-16 00:43:52 +0200

    feat: implement adding ship

---
+++ src/controllers/addShips.ts
@@ -0,0 +1,26 @@
+import { WebSocket } from 'ws';
+import { AddShipsRequest } from '../types';
+import responseWrapper from '../utils/responseWrapper';
+import gameDb from '../database/game';
+import wsDb from '../database/wsDb';
+
+const addShip = (ws: WebSocket, dataInfo: AddShipsRequest) => {
+  gameDb.addShips(dataInfo);
+  const secondPlayerId = gameDb.getSecondPlayerOfGame(dataInfo.indexPlayer);
+  console.log('secondPlayerId', secondPlayerId);
+  if (secondPlayerId && gameDb.hasSecondPlayerShips(secondPlayerId)) {
+    console.log('Condition is working');
+    const ws2 = wsDb.getWsByUserId(secondPlayerId);
+    console.log('answer to player 1', gameDb.startGame(dataInfo.indexPlayer));
+    ws.send(
+      responseWrapper(gameDb.startGame(dataInfo.indexPlayer), 'start_game'),
+    );
+    console.log('answer to player 2', gameDb.startGame(secondPlayerId));
+    ws2.send(responseWrapper(gameDb.startGame(secondPlayerId), 'start_game'));
+    gameDb.setTurn(dataInfo.indexPlayer, false);
+    const turn = gameDb.turn(dataInfo.gameId);
+    ws.send(responseWrapper(turn, 'turn'));
+    ws2.send(responseWrapper(turn, 'turn'));
+  }
+};
+export default addShip;
