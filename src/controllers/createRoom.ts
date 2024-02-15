109b8dd66f0de4ff29a05a43dbce132b5a323c7e
Iryna Yehorova <interest4me@gmail.com>
2024-02-16 00:42:51 +0200

    feat: implement create room

---
+++ src/controllers/createRoom.ts
@@ -0,0 +1,17 @@
+import { WebSocket } from 'ws';
+
+import roomsDb from '../database/rooms';
+import responseWrapper from '../utils/responseWrapper';
+import wsDb from '../database/wsDb';
+
+const createRoom = (ws: WebSocket, clients: Set<WebSocket>) => {
+  const userInfo = wsDb.getUserInfoByWs(ws);
+  console.log('userInfo', userInfo);
+  roomsDb.createNewRoom(userInfo);
+  const updateRoomResponse = roomsDb.updateRoomState();
+  for (const client of clients) {
+    client.send(responseWrapper(updateRoomResponse, 'update_room'));
+  }
+};
+
+export default createRoom;
